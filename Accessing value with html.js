932455c0dfeddbe2464835be4eefd516fcf36850
Mudathir-Ajibike <nabduganiyu@gmail.com>
2025-07-16 22:48:45 +0100

    sixth commit

---
+++ Accessing value with html.js
@@ -0,0 +1,5 @@
+var player=["Adagular", "Mbappe", "Naymar", "Rudriga"]
+document.getElementById("demo").innerHTML = player[0];
+document.getElementById("demo1").innerHTML = player;
+document.getElementById("demo2").innerHTML = player. join("-")
+document.getElementById("demo3").innerHTML = player. pop();
